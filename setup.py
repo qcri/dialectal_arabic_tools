3708d5ca541e3b572f78b02b8f43dc6581f5ec19
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-09-23 13:43:41 +0300

    create package data to provide the keras models and example data

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'numpy',
     'Click>=6.0',
-    'keras',
     'tensorflow',
+    'h5py',
     # TODO: put package requirements here
 ]
 
@@ -29,13 +29,18 @@ test_requirements = [
 
 setup(
     name='dialectal_arabic_tools',
-    version='0.1.2',
+    version='0.0.1a9',
     description="Dialectal Arabic Tools comprises the different modules developed in Qatar Computing Research Institute (QCRI) developed by the ALT team to handle Dialectal Arabic Segmentation, POS tagging, daicrtization and more",
     long_description=readme + '\n\n' + history,
     author="Mohamed Eldesouki",
     author_email='disooqi@gmail.com',
     url='https://github.com/qcri/dialectal_arabic_tools',
     packages=find_packages(include=['dialectal_arabic_tools']),
+    #package_dir={'mypkg': 'src/mypkg'},
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
+    # If using the setuptools-specific include_package_data argument, files specified by package_data will not be
+    # automatically added to the manifest unless they are listed in the MANIFEST.in file.)
+    # package_data={'dialectal_arabic_tools': ['data/*']},
     entry_points={
         'console_scripts': [
             'dialectal_arabic_tools=dialectal_arabic_tools.cli:main'
@@ -43,7 +48,11 @@ setup(
     },
     include_package_data=True,
 
-    zip_safe=False,
+    # data_files=[('bitmaps', ['bm/b1.gif', 'bm/b2.gif']),
+    #               ('config', ['cfg/data.cfg']),
+    #               ('/etc/init.d', ['init-script'])],
+
+    zip_safe=True,
     keywords='dialectal_arabic_tools',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -57,8 +66,14 @@ setup(
     tests_require=test_requirements,
     setup_requires=setup_requirements,
 
-    dependency_links=['git+git://github.com/phipleg/keras@crf#egg=keras'],
-# dependency_links = ['git+https://github.com/liamzebedee/scandir.git#egg=scandir-0.1'],
-#     install_requires = ['scandir'],
+    #http://peak.telecommunity.com/DevCenter/setuptools#dependencies-that-aren-t-in-pypi
+
+    dependency_links=[
+        # git+git://github.com/phipleg/keras@crf#egg=keras,
+        # 'https://github.com/phipleg/keras/tree/crf#egg=keras-crf-2.0.6',
+        # 'https://github.com/phipleg/keras/archive/crf.zip#egg=keras-99.99',
+                      ],
+    # dependency_links = ['git+https://github.com/liamzebedee/scandir.git#egg=scandir-0.1'],
+    #     install_requires = ['scandir'],
     install_requires=requirements,
 )
