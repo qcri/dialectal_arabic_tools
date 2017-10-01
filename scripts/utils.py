bf1322c764b1e5d8dd586f8c23b0a99846429083
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-10-01 14:46:50 +0300

    implement function from_plain_to_conll

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 # Generate CoNLL format
+import os
 import codecs
 import numpy as np
 from collections import Counter
 from itertools import chain
-from sklearn.model_selection import train_test_split
-from keras.preprocessing import sequence
+# from sklearn.model_selection import train_test_split
+# from keras.preprocessing import sequence
 
 __author__ = 'disooqi'
 
@@ -39,47 +40,78 @@ __author__ = 'disooqi'
 #         else:
 #             conll.write('=======================================')
 
-def _fit_term_index(terms, reserved=[], preprocess=lambda x: x):
-    # todo I need to know that this is working well
-    all_terms = chain(*terms)
-    all_terms = map(preprocess, all_terms)
-    term_freqs = Counter(all_terms).most_common()
-    id2term = reserved + [term for term, tf in term_freqs]
-    return id2term
+# def _fit_term_index(terms, reserved=[], preprocess=lambda x: x):
+#     # todo I need to know that this is working well
+#     all_terms = chain(*terms)
+#     all_terms = map(preprocess, all_terms)
+#     term_freqs = Counter(all_terms).most_common()
+#     id2term = reserved + [term for term, tf in term_freqs]
+#     return id2term
+#
+#
+# def _invert_index(id2term):
+#     return {term: i for i, term in enumerate(id2term)}
+#
+#
+# def split_dataset(file_path, test_size=0.2, shuffle=True):
+#     SEG_TAGS = ('S', 'B', 'E', 'M')
+#     words, target_of_words = None, None
+#     with codecs.open(file_path, encoding='utf-8') as conell:
+#         list_of_lines = [line.strip().split() for line in conell if len(line.strip().split()) == 2]
+#         chars, targets = list(zip(*list_of_lines))
+#         words = ''.join(chars).split('WB')
+#         target_of_words = ''.join(targets).split('WB')
+#
+#         words = [tuple(word) for word in words]
+#         target_of_words = [tuple(trg) for trg in target_of_words]
+#
+#     index2word = _fit_term_index(words, reserved=['<PAD>', '<UNK>'])
+#     word2index = _invert_index(index2word)
+#
+#     index2pos = SEG_TAGS
+#     pos2index = _invert_index(index2pos)
+#
+#     X_train_idx = np.array([[word2index[w] for w in words] for words in words])
+#     y_train_idx = np.array([[pos2index[t] for t in pos_tags] for pos_tags in target_of_words])
+#
+#     X = sequence.pad_sequences(X_train_idx, maxlen=50, padding='post')
+#     y_train = sequence.pad_sequences(y_train_idx, maxlen=50, padding='post')
+#     y = np.expand_dims(y_train, -1)
+#
+#     return train_test_split(X, y, test_size=test_size, shuffle=shuffle)
 
 
-def _invert_index(id2term):
-    return {term: i for i, term in enumerate(id2term)}
+def from_plain_to_conll(dir_path):
+    rootDir = dir_path
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('Found directory: %s' % dirName)
+        for i in range(1, 6):
+            with codecs.open('joint.trian.' + str(i), encoding='utf-8', mode='w') as jointObj:
+                for fname in fileList:
+                    if 'train' in fname and str(i) in fname:
+                        with codecs.open(os.path.join(dirName, fname), encoding='utf-8') as rObj:
+                            for line in rObj:
+                                #                             print line
+                                jointObj.write(line)
 
+            with codecs.open('joint.dev.' + str(i), encoding='utf-8', mode='w') as jointObj:
+                for fname in fileList:
+                    if 'dev' in fname and str(i) in fname:
+                        with codecs.open(os.path.join(dirName, fname), encoding='utf-8') as rObj:
+                            for line in rObj:
+                                jointObj.write(line)
 
-def split_dataset(file_path, test_size=0.2, shuffle=True):
-    SEG_TAGS = ('S', 'B', 'E', 'M')
-    words, target_of_words = None, None
-    with codecs.open(file_path, encoding='utf-8') as conell:
-        list_of_lines = [line.strip().split() for line in conell if len(line.strip().split()) == 2]
-        chars, targets = list(zip(*list_of_lines))
-        words = ''.join(chars).split('WB')
-        target_of_words = ''.join(targets).split('WB')
-
-        words = [tuple(word) for word in words]
-        target_of_words = [tuple(trg) for trg in target_of_words]
-
-    index2word = _fit_term_index(words, reserved=['<PAD>', '<UNK>'])
-    word2index = _invert_index(index2word)
-
-    index2pos = SEG_TAGS
-    pos2index = _invert_index(index2pos)
-
-    X_train_idx = np.array([[word2index[w] for w in words] for words in words])
-    y_train_idx = np.array([[pos2index[t] for t in pos_tags] for pos_tags in target_of_words])
-
-    X = sequence.pad_sequences(X_train_idx, maxlen=50, padding='post')
-    y_train = sequence.pad_sequences(y_train_idx, maxlen=50, padding='post')
-    y = np.expand_dims(y_train, -1)
-
-    return train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+            with codecs.open('joint.test.' + str(i), encoding='utf-8', mode='w') as jointObj:
+                for fname in fileList:
+                    if 'test' in fname and str(i) in fname:
+                        with codecs.open(os.path.join(dirName, fname), encoding='utf-8') as rObj:
+                            for line in rObj:
+                                jointObj.write(line)
+        else:
+            break
 
 
 if __name__ == '__main__':
-    split = split_dataset(r'../files/all.trg.conll')
-    print(len(split))
+    from_plain_to_conll(r'/home/disooqi/Dropbox/most_cited/all_data/joint')
+    # split = split_dataset(r'../files/all.trg.conll')
+    # print(len(split))
